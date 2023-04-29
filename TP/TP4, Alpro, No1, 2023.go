1fa444f5eaba7f1f1ea23d6fc909bb59f0213f3d
Somus88 <96776245+Somus88@users.noreply.github.com>
2023-04-29 21:42:01 +0700

    Update TP4, Alpro, No1, 2023.go

--- TP/TP4, Alpro, No1, 2023.go
+++ TP/TP4, Alpro, No1, 2023.go
@@ -5,7 +5,7 @@ import (
 	"math"
 )
 
-func z_1303220104(x int, y int) float64 {
+func z(x int, y int) float64 {
 	var z1 float64
 	z1 = math.Sqrt(((3 * float64(x)) * (6 * float64(y)) / (4 * float64(y))))
 	return z1
@@ -14,5 +14,5 @@ func z_1303220104(x int, y int) float64 {
 func main() {
 	var a, b int
 	fmt.Scanln(&a, &b)
-	fmt.Println(z_1303220104(a, b), z_1303220104(b, a))
+	fmt.Println(z(a, b), z(b, a))
 }
