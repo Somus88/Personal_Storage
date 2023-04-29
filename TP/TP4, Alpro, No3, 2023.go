f96eff02ecfb6e7007b66cd402b2af1b724566e9
Somus88 <96776245+Somus88@users.noreply.github.com>
2023-04-29 21:44:24 +0700

    Update and rename nomer3.go to TP4, Alpro, No3, 2023.go

--- TP/TP4, Alpro, No3, 2023.go
+++ TP/TP4, Alpro, No3, 2023.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func cumlaude_1303220104(ipk float64, masaStudi int, publikasi bool) bool {
+func cumlaude(ipk float64, masaStudi int, publikasi bool) bool {
 	var kriteria bool
 	if ipk >= 3.51 && ipk <= 4.00 && masaStudi <= 8 && publikasi == true {
 		kriteria = true
@@ -10,7 +10,7 @@ func cumlaude_1303220104(ipk float64, masaStudi int, publikasi bool) bool {
 	return kriteria
 }
 
-func sangatMemuaskan_1303220104(ipk float64, masaStudi int, publikasi bool) bool {
+func sangatMemuaskan(ipk float64, masaStudi int, publikasi bool) bool {
 	var kriteria bool
 	if ipk >= 2.76 && ipk <= 3.5 && masaStudi <= 14 && (publikasi == true || publikasi == false) {
 		kriteria = true
@@ -18,7 +18,7 @@ func sangatMemuaskan_1303220104(ipk float64, masaStudi int, publikasi bool) bool
 	return kriteria
 }
 
-func memuaskan_1303220104(ipk float64, masaStudi int, publikasi bool) bool {
+func memuaskan(ipk float64, masaStudi int, publikasi bool) bool {
 	var kriteria bool
 	if ipk >= 2.00 && ipk <= 2.75 && masaStudi <= 14 && (publikasi == true || publikasi == false) {
 		kriteria = true
@@ -31,11 +31,11 @@ func main() {
 	var y int
 	var z bool
 	fmt.Scanln(&x, &y, &z)
-	if cumlaude_1303220104(x, y, z) == true {
+	if cumlaude(x, y, z) == true {
 		fmt.Println("Cumlaude")
-	} else if memuaskan_1303220104(x, y, z) == true {
+	} else if memuaskan(x, y, z) == true {
 		fmt.Println("Memuaskan")
-	} else if sangatMemuaskan_1303220104(x, y, z) == true || cumlaude_1303220104(x, y, z) == false {
+	} else if sangatMemuaskan(x, y, z) == true || cumlaude(x, y, z) == false {
 		fmt.Println("Sangat memuaskan")
 	}
 }
