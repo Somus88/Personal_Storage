110cdffc9e12aa9fd945c7a4f6e8778cba232a8f
Somus88 <96776245+Somus88@users.noreply.github.com>
2023-10-08 18:56:11 +0700

    Create list.h

---
+++ TP/Sem3/no2/list.h
@@ -0,0 +1,30 @@
+#ifndef LIST_H_INCLUDED
+#define LIST_H_INCLUDED
+#include <iostream>
+#define first(L) L.first
+#define next(P) P->next
+#define info(P) P->info
+using namespace std;
+typedef int infotype;
+typedef struct elmlist *address;
+
+struct elmlist
+{
+    infotype info;
+    address next;
+};
+struct List
+{
+    address first;
+};
+
+void createList(List &L);
+address allocate(infotype x);
+void insertFirst(List &L, address P);
+void insertLast(List &L, address P);
+void insertAfter(List &L, address prec, address P);
+void deleteLast(List &L, address P);
+void deleteAfter(List &L, address prec, address P);
+void printInfo(List L);
+
+#endif // LIST_H_INCLUDED
