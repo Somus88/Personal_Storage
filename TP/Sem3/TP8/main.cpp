45792a2819314b0f95a2c08e0e4f7e5331ebfdc9
Somus88 <96776245+Somus88@users.noreply.github.com>
2023-11-04 19:40:27 +0700

    Create main.cpp

---
+++ TP/Sem3/TP8/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "doubleCircular.h"
+using namespace std;
+
+int main()
+{
+    List L;
+    List L2;
+    createList(L);
+    insertFirst(L, alokasi(1));
+    insertLast(L, alokasi(5));
+    insertLast(L, alokasi(15));
+    insertLast(L, alokasi(25));
+    insertLast(L, alokasi(35));
+    insertLast(L, alokasi(45));
+    insertLast(L, alokasi(55));
+    insertLast(L, alokasi(65));
+    insertLast(L, alokasi(75));
+    insertLast(L, alokasi(85));
+    insertAfter(L, findElm(L, 75), alokasi(80));
+    printInfo(L);
+
+    deleteByValue(L,5);
+    deleteByValue(L,100);
+    deleteByValue(L,45);
+    deleteByValue(L,85);
+
+    printInfo(L);
+
+    createList(L2);
+    reverseList(L,L2);
+    printInfo(L2);
+    return 0;
+}
