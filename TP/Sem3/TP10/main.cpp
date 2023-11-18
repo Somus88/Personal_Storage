d69900abfa5c9c9e130cb7d8b47a77f7326e3b0b
Somus88 <96776245+Somus88@users.noreply.github.com>
2023-11-18 23:43:49 +0700

    Create main.cpp

---
+++ TP/Sem3/TP10/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "queue.h"
+
+using namespace std;
+
+int main()
+{
+    queue tugas, QGenap, QGanjil;
+    address emptyThis;
+    cout << "Cek awal" << endl;
+    createQueue(tugas);
+    enQueue(tugas, alokasi(2));
+    enQueue(tugas, alokasi(3));
+    enQueue(tugas, alokasi(4));
+    enQueue(tugas, alokasi(5));
+    enQueue(tugas, alokasi(6));
+    enQueue(tugas, alokasi(7));
+    enQueue(tugas, alokasi(8));
+    enQueue(tugas, alokasi(9));
+    enQueue(tugas, alokasi(10));
+    enQueue(tugas, alokasi(11));
+    printInfo(tugas);
+    cout << endl << "Ganjil Genap" << endl << "Kondisi awal" << endl;
+    createQueue(QGanjil);
+    createQueue(QGenap);
+    printInfo(tugas);
+    ganjilGenap(tugas, QGanjil, QGenap);
+    cout << endl << "Kondisi Akhir" << endl;
+    printInfo(tugas);
+    cout << endl << "Queue Ganjil" << endl;
+    printInfo(QGanjil);
+    cout << endl << "Queue Genap" << endl;
+    printInfo(QGenap);
+    return 0;
+}
