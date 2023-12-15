1bd8750ad93862c96bb3b4beb5e956ce348a14f0
Somus88 <96776245+Somus88@users.noreply.github.com>
2023-12-16 00:44:44 +0700

    Create main.cpp

---
+++ TP/Sem3/TP14/main.cpp
@@ -0,0 +1,13 @@
+//tipe soal genap
+#include <iostream>
+#include "graf.h"
+using namespace std;
+
+int main()
+{
+    listOfVertex G;
+    initGraph(G);
+    buildGraph(G);
+    showGraph(G);
+    return 0;
+}
